0d53e576a24046a2758efe4b0e7e267020f0c985
Eugen Neuber <eugen.neuber@tugraz.at>
2022-09-19 16:00:51 +0200

    Add micro modal /WIP

--- src/dbp-overview-app.js
+++ src/dbp-overview-app.js
@@ -3,12 +3,15 @@ import {
     searchBox,
     configure,
     hits,
-    refinementList, /*currentRefinements,*/
+    refinementList,
+    currentRefinements,
     hierarchicalMenu,
     stats,
     pagination,
 } from 'instantsearch.js/es/widgets';
 import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
+
+import MicroModal from './micromodal.es';
 
 import { licenses } from '../assets/licenses/spdx.json';
 const spdxLicenses = licenses;
@@ -88,27 +91,27 @@ export function init(typesenseConfig, dateFilter, privatePath) {
                     //     }
                     // });
 
-                    console.log('blueprints for ' + item.name);
+                    // console.log('blueprints for ' + item.name);
                     // console.dir(item.blueprint);
                     // console.dir([refinedBlueprints, nonRefinedBlueprints]);
                     // console.dir(item);
 
                     return `
-        <div style="position: relative; height: 100%; width: 100%">
-          <div class="hit-name">
+        <div style="position: relative; height: 100%; width: 100%" onclick="console.log('${item.id}');MicroModal.show('detail-${item.id}');">
+            <div class="hit-name">
             <img class="hit-name-img" src=${item.link_icon || `${privatePath}/icons8-missing-32.png`} alt="link">
             ${item._highlightResult.name.value}
             ${item.labs.includes('yes') ? `<img class="labs-img" src="${privatePath}/lab_flask.svg" alt="labs">` : ''}
-          </div>
-          <div class="" style="width:100%; height:3rem;text-overflow: ellipsis">${item._highlightResult.description.value}</div>
-          <div class="hit-content-type">
-            <div>This component is used in this blueprints:</div>
-            <select style="width:100%;margin:5px;"
+            </div>
+            <div class="" style="width:100%; height:4.5rem;text-overflow: ellipsis">${item._highlightResult.description.value}</div>
+            <div class="hit-content-type">
+                <div>This component is used in this blueprints:</div>
+                <select style="width:100%;margin:5px;"
                     size="${Math.min(3, Math.max(3, item._highlightResult.blueprint.length))}">
-            ${item._highlightResult.blueprint.map(a => `<option value="${a.value}">${a.value}</option>`)}
-            </select>
-          </div>
-          <div class="hit-types">
+                    ${item._highlightResult.blueprint.map(a => `<option value="${a.value}">${a.value}</option>`)}
+                </select>
+            </div>
+            <div class="hit-types">
             <div class="hit-types-subtypes">
               <div class="hit-document-type">
                 <div>Type of project:</div>
@@ -118,15 +121,16 @@ export function init(typesenseConfig, dateFilter, privatePath) {
                 <div>Subtype of project:</div>
                 ${item._highlightResult.content_type.map(a => `<div class="type ${a.value.replace(/\s+/g, '-').toLowerCase()}" onclick="document.getElementById('${'content-type-'+a.value}').click();">${a.value}</div>`).join('')}
               </div>
+            </div>
           </div>
-          <div class="hit-used-programming-languages">
+            <div class="hit-used-programming-languages">
             <div>Used programming languages:</div>
             ${item._highlightResult.used_programming_languages.map(a => `<div class="type ${a.value.replace(/\s+/g, '-').toLowerCase()}" onclick="document.getElementById('${'prog-lang-'+a.value}').click();">${a.value}</div>`).join('')}
           </div>
-          <div class="hit-license">License: ${item.license.map(l => '<span>' + formatLicense(l) + '</span>').join('')}</div>
-          ${item.release_date > 0 ? `<div class="hit-release">Release date: ${formattedTime}</div>` : ''}
-          ${item.release_version ? `<div class="hit-release">Version <span class="release">${item.release_version}</span></div>` : ''}
-          <div class="links">
+            <div class="hit-license">License: ${item.license.map(l => '<span>' + formatLicense(l) + '</span>').join('')}</div>
+            ${item.release_date > 0 ? `<div class="hit-release">Release date: ${formattedTime}</div>` : ''}
+            ${item.release_version ? `<div class="hit-release">Version <span class="release">${item.release_version}</span></div>` : ''}
+            <div class="links">
               ${ item.link_repo || item.link_doc || item.link_demo || item.link_changelog ? `<span class="hit-links">Links:</span>` : ''}
               <span class="hit-repo">${item.link_repo ? `<a href=${item.link_repo} rel="noopener noreferrer" target="_blank"><img src="${privatePath}/Git-Icon-Black.png" alt="repository" title="repository"></a>` : ''}</span>
               <span class="hit-doc">${item.link_doc ? `<a href=${item.link_doc} rel="noopener noreferrer" target="_blank"><img src="${privatePath}/icons8-book-60.png" alt="documentation" title="documentation"></a>` : ''}</span>
@@ -136,7 +140,43 @@ export function init(typesenseConfig, dateFilter, privatePath) {
             <!--
             <div class="hit-rating">[${item.sort}]</div>
             -->
-          ${ isNew ? `<div class="ribbon ribbon-bottom-right"><span>new</span></div>` : ''}
+            ${ isNew ? `<div class="ribbon ribbon-bottom-right"><span>new</span></div>` : ''}
+        </div>
+        <div class="modal micromodal-slide" id="detail-${item.id}" aria-hidden="true">
+            <div class="modal-overlay" tabindex="1" data-micromodal-close>
+                <div class="modal-container"
+                     id="edit-sender-modal-box-${item.id}"
+                     role="dialog"
+                     aria-modal="true"
+                     aria-labelledby="edit-sender-modal-title">
+                    <header class="modal-header" style="display: flex; flex-direction: row; justify-content: space-between">
+                        <h3>${item.name}</h3>
+                        <button style="height: 1.2rem;"
+                                data-micromodal-close
+                                title="close"
+                                class="modal-close"
+                                aria-label="Close modal"
+                                onclick="console.log('x'); MicroModal.close('detail-${item.id}');">
+                           <svg style="width: 1rem;" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                        </button>
+                    </header>
+                    <main class="modal-content">
+                        <div class="modal-content-item">
+                            <p>${item.description}</p>
+                        </div>
+                    </main>
+                    <footer class="modal-footer">
+                        <div class="modal-footer-btn">
+                            <button class="button"
+                                    data-micromodal-close
+                                    aria-label="Close this dialog window"
+                                    onclick="console.log('close'); MicroModal.close('detail-${item.id}');">
+                                close
+                            </button>
+                        </div>
+                    </footer>
+                </div>
+            </div>
         </div>
       `;
                 },
@@ -210,22 +250,32 @@ export function init(typesenseConfig, dateFilter, privatePath) {
             },
             showParentLevel: false,
         }),
-        // currentRefinements({
-        //     container: '#current-blueprints',
-        //     includedAttributes: ['blueprint', 'maintained_by'],
-        //     cssClasses: { list: ['flex']},
-        //     transformItems: function (items) {
-        //         return items.map(item => {
-        //             item.refinements = item.refinements.map(i => {
-        //                 const nameParts = i.value.split('(');
-        //                 i.label = nameParts.shift();
-        //
-        //                 return i;
-        //             });
-        //             return item;
-        //         });
-        //     },
-        // }),
+        currentRefinements({
+            container: '#current-refinements',
+            includedAttributes: [
+                'target_audience',
+                'document_type',
+                'content_type',
+                'maintained_by',
+                'license',
+                'used_programming_languages',
+                'blueprint',
+                'hierarchicalBlueprints.lvl0',
+                'hierarchicalBlueprints.lvl1'
+            ],
+            cssClasses: { list: ['flex']},
+            transformItems: function (items) {
+                return items.map(item => {
+                    item.refinements = item.refinements.map(i => {
+                        const nameParts = i.value.split('(');
+                        i.label = nameParts.shift();
+
+                        return i;
+                    });
+                    return item;
+                });
+            },
+        }),
         refinementList({
             container: '#document-type-list',
             attribute: 'document_type',
